3b29d88ed82922cc14394fa47707269962dcef2d
KadirKarabacak <kadirht@hotmail.com>
2024-10-03 18:31:55 +0300

    feat: add icons from fontawesome

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCartPlus, faTrash, faMinus, faPlus, faTimes, faSearch, faArrowRight, faHome, faCartShopping, faArchive } from '@fortawesome/free-solid-svg-icons'
+import { faCartPlus, faTrash, faMinus, faPlus, faTimes, faSearch, faArrowRight, faHome, faCartShopping, faArchive, faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -8,7 +8,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 // İstediğiniz ikonları kütüphaneye ekleyin
-library.add(faCartPlus, faTrash, faMinus, faPlus, faTimes, faSearch, faArrowRight, faHome, faCartShopping, faArchive)
+library.add(faCartPlus, faTrash, faMinus, faPlus, faTimes, faSearch, faArrowRight, faHome, faCartShopping, faArchive, faBars)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
