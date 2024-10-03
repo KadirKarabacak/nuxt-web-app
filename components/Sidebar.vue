3f69cf40a56735292f2d882d76f08a4aeb4637cc
KadirKarabacak <kadirht@hotmail.com>
2024-10-03 12:47:44 +0300

    fix: fix sidebar stylings

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-2/12 bg-stone-800 text-white p-4">
+    <div class="w-2/12 bg-stone-800 text-white py-4 px-2">
       <div class="aspect-square mb-12">
         <img
           class="block w-full h-full"
@@ -8,21 +8,21 @@
         />
       </div>
       <ul class="flex flex-col gap-2">
-        <li :class="{'bg-stone-600': isActive('/'), 'border-green-400 border-b-2': isActive('/'), 'py-4 rounded-sm pl-4': true}">
+        <li :class="{'bg-stone-600': isActive('/'), '!border-green-400 border-b-4': isActive('/'), 'py-4 rounded-md flex justify-center border-b-4 border-transparent': true}">
           <span class="flex items-center">
             <client-only>
               <font-awesome-icon class=text-green-400 :icon="['fas', 'home']" /> <n-button f type="text" class="text-green-400 font-bold" @click="goToHome">Home</n-button>
             </client-only>
           </span>
         </li>
-        <li :class="{'bg-stone-600': isActive('/products'), 'border-green-400 border-b-2': isActive('/products'), 'py-4 rounded-md pl-4': true}">
+        <li :class="{'bg-stone-600': isActive('/products'), '!border-green-400 border-b-4': isActive('/products'), 'py-4 rounded-md flex justify-center border-b-4 border-transparent': true}">
           <span class="flex items-center">
             <client-only>
               <font-awesome-icon class="text-green-400" icon="archive" /> <n-button type="text" class="text-green-400 font-bold" @click="goToProducts">Products</n-button>
             </client-only>
           </span>
         </li>
-        <li :class="{'bg-stone-600': isActive('/orders'), 'border-green-400 border-b-2': isActive('/orders'),'py-4 rounded-md pl-4': true}">
+        <li :class="{'bg-stone-600': isActive('/orders'), '!border-green-400 border-b-4': isActive('/orders'),'py-4 rounded-md flex justify-center border-b-4 border-transparent': true}">
           <span class="flex items-center">
             <client-only>
               <font-awesome-icon class="text-green-400" icon="cart-shopping" /> <n-button type="text" class="text-green-400 font-bold" @click="goToOrders">Orders</n-button>
